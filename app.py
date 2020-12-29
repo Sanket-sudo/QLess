4cb866f902a4a63be9ae86c0921041524acfa5ce
shreyakothavale <37474463+shreyakothavale@users.noreply.github.com>
2020-12-29 13:22:36 +0530

    added recommendation function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 
+import aprioriRec as ar 
 
 app = Flask(__name__)
 
@@ -7,4 +8,16 @@ app = Flask(__name__)
 @app.route('/recommend', methods=['POST'])
 def recommend():
 	data = request.get_json()
-	return data['item']
+	#return data['item']
+	return redirect(url_for('products',prod_item = data['item']))
+	
+@app.route('/products/<prod_item>')
+def recommendation(prod_id):
+    # call recommendation function 
+    product_list = list(ar.getItems(prod_item))
+    return jsonify({"product_list":product_list})
+	
+'''	
+if __name__ == "__main__":
+	app.run()
+'''
